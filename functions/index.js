0f289575daa86228917bf265294011742f3e24c4
ayrbox <srdx7@hotmail.com>
2020-05-27 23:38:52 +0100

    Use smtp instead of api.

--- functions/index.js
+++ functions/index.js
@@ -1,43 +1,59 @@
 const functions = require('firebase-functions');
 const express = require('express');
+const nodemailer = require('nodemailer');
+
 const app = express();
-const mailgun = require('mailgun-js');
+
+const DEST = 'info@summitwealth.co.uk';
+
+// Read keys from firebase config https://firebase.google.com/docs/functions/config-env
+// `firebase functions:config:get`
+const {
+  mailgun: { smtp, user, password },
+} = functions.config();
+
+const transporter = nodemailer.createTransport({
+  host: smtp,
+  auth: {
+    user: user,
+    pass: password,
+  },
+});
 
 app.post('/contact-email', (req, res) => {
   const { name, email, phone, message } = req.body;
 
-  const mailText = `
-    Name: ${name}
-    Email: ${email}
-    Phone: ${phone}
-    Message: ${message || ' - '}
-  `;
+  const mailHtml = `
+      <p>
+        You have got contact request via summitwealth.co.uk for follow up. Please find the contact detail below.
+      </p>
+      <hr />
+      Name: <strong>${name}</strong>
+      <br/>
+      Email: <strong>${email}</strong>
+      <br/>
+      Phone: <strong>${phone}</strong>
+      <br/>
+      Message:
+      <br />
+      <p>${message || ' - '}</p>
+   `;
 
-  const data = {
+  const mailOptions = {
     from: `${name} <${email}>`,
-    to: 'srdx7@hotmail.com',
+    to: DEST,
     subject: `Contact from ${name}`,
-    text: mailText,
+    html: mailHtml,
   };
 
-  // Read keys from firebase config https://firebase.google.com/docs/functions/config-env
-  // `firebase functions:config:get`
-  const apiKey = functions.config().mailgun.api_key;
-  const domain = functions.config().mailgun.domain;
-
-  const mg = mailgun({ apiKey, domain });
-  mg.messages().send(data, function(error, body) {
+  return transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      res.status(500).send({
+      return res.status(500).send({
         msg: 'Error sending email.',
-        detail: error,
+        detail: error
       });
-      return;
     }
-
-    res.end({
-      msg: 'Email sent.',
-    });
+    return res.send('Email sent.');
   });
 });
 
